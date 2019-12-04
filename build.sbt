b01d3053e68172897bcc07e6b40cef4232b25991
Damian Fiłonowicz <damian.filonowicz@gmail.com>
2019-12-04 16:08:11 +0100

    Add config and name mapping

--- build.sbt
+++ build.sbt
@@ -1,11 +1,15 @@
 name := "AreYouAfraidOfTheDark"
 
-version := "0.2"
+version := "0.3"
 
 scalaVersion := "2.11.12"
 
 // https://mvnrepository.com/artifact/org.bytedeco/opencv
 libraryDependencies += "org.bytedeco" % "opencv" % "4.1.2-1.5.2"
+
+// https://mvnrepository.com/artifact/com.typesafe/config
+libraryDependencies += "com.typesafe" % "config" % "1.4.0"
+
 
 //// https://mvnrepository.com/artifact/org.apache.spark/spark-core
 //libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.4"
--- src/main/scala/OpenCV.scala
+++ src/main/scala/OpenCV.scala
@@ -1,28 +1,38 @@
 import java.io.File
 
+import com.typesafe.config._
 import org.opencv.core.Core.{NATIVE_LIBRARY_NAME, mean}
 import org.opencv.imgcodecs.Imgcodecs.imread
-import org.opencv.imgproc.Imgproc.{cvtColor, COLOR_BGR2HSV}
+import org.opencv.imgproc.Imgproc.{COLOR_BGR2HSV, cvtColor}
 
 object OpenCV {
   System.loadLibrary(NATIVE_LIBRARY_NAME)
 
-  def findV(path: String): Double = {
+  def findV(path: String): Int = {
     val picture = imread(path)
     cvtColor(picture, picture, COLOR_BGR2HSV, 3)
     val averageV = mean(picture).`val`(2)
     val normalizedV = (1 - averageV / 255) * 100
-    normalizedV
+    normalizedV.toInt
   }
 
   def main(args: Array[String]): Unit = {
-    val input = "/photos/images"
 
-    val sources = new File(input).listFiles.par
-    val values = sources.map(x => findV(x.toString).floor)
+    val conf = ConfigFactory.load()
+    val input = conf.getString("conf.in")
+    val output = conf.getString("conf.out")
 
-    println(sources)
-    println(values)
-    println(values.length)
+    val sources = new File(input).listFiles.par.map(x => x.toString)
+
+    val z = sources.map(x => {
+      val value = findV(x)
+      val label = if (value >= args(0).toInt) "dark" else "bright"
+      val name = x.split("\\.", 2)
+      name(0).concat("_" + label + "_" + value + "." + name(1))
+    })
+
+
+    println(z)
+    println(z.length)
   }
 }
