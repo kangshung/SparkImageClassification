186ea1f01c3b59df955a597899d18b9a2e85ed53
Damian Fiłonowicz <damian.filonowicz@gmail.com>
2020-02-09 19:00:15 +0100

    Add Spark solution

--- build.sbt
+++ build.sbt
@@ -1,21 +1,23 @@
 name := "AreYouAfraidOfTheDark"
 
-version := "0.3"
+version := "0.4"
 
 scalaVersion := "2.11.12"
 
-// https://mvnrepository.com/artifact/org.bytedeco/opencv
-libraryDependencies += "org.bytedeco" % "opencv" % "4.1.2-1.5.2"
+// https://mvnrepository.com/artifact/org.apache.spark/spark-core
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.5"
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-sql
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.5"
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-mllib
+libraryDependencies += "org.apache.spark" %% "spark-mllib" % "2.4.5" % "runtime"
 
 // https://mvnrepository.com/artifact/com.typesafe/config
 libraryDependencies += "com.typesafe" % "config" % "1.4.0"
 
+//resolvers += "MMLSpark Repo" at "https://mmlspark.azureedge.net/maven"
+//libraryDependencies += "com.microsoft.ml.spark" %% "mmlspark" % "1.0.0-rc1"
 
-//// https://mvnrepository.com/artifact/org.apache.spark/spark-core
-//libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.4"
-//
-//// https://mvnrepository.com/artifact/org.apache.spark/spark-sql
-//libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.4"
-//
-//// https://mvnrepository.com/artifact/org.apache.spark/spark-mllib
-//libraryDependencies += "org.apache.spark" %% "spark-mllib" % "2.4.4"
+// https://mvnrepository.com/artifact/org.bytedeco/opencv
+libraryDependencies += "org.bytedeco" % "opencv" % "4.1.2-1.5.2"
--- src/main/scala/OpenCV.scala
+++ src/main/scala/OpenCV.scala
@@ -1,55 +1,73 @@
-import java.io.File
 import java.nio.file.{Files, Paths, StandardCopyOption}
-
-import com.typesafe.config.ConfigFactory
-
+import com.typesafe.config.{Config, ConfigFactory}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.expressions.UserDefinedFunction
+import org.apache.spark.sql.functions.udf
 import org.opencv.core.Core.{NATIVE_LIBRARY_NAME, mean}
-import org.opencv.imgcodecs.Imgcodecs.imread
-import org.opencv.imgproc.Imgproc.{COLOR_BGR2HSV, cvtColor}
-
-import scala.collection.parallel.mutable.ParArray
-
+import org.opencv.core.{CvType, Mat}
+import org.opencv.imgproc.Imgproc.{COLOR_BGR2HSV, COLOR_GRAY2BGR, cvtColor}
 
 object OpenCV {
-  System.loadLibrary(NATIVE_LIBRARY_NAME)
+  System.loadLibrary(NATIVE_LIBRARY_NAME) // load OpenCV
+  val dropLeft = 7 // drop file://
+  val config: Config = ConfigFactory.load() // load resources/reference.conf
+  val input: String = config.getString("conf.in")
+  val output: String = config.getString("conf.out")
 
-  def findV(path: String): Int = {
-    val picture = imread(path)
-    cvtColor(picture, picture, COLOR_BGR2HSV, 3)
-    val averageV = mean(picture).`val`(2)
+  def findV(height: Int, width: Int, channel: Int, data: Array[Byte]): Int = {
+    val image = new Mat(height, width, CvType.CV_8UC(channel))
+    image.put(0, 0, data)
+
+    if (image.channels() == 1)
+      cvtColor(image, image, COLOR_GRAY2BGR, 3)
+
+    cvtColor(image, image, COLOR_BGR2HSV, 3)
+    val averageV = mean(image).`val`(2)
     val normalizedV = (1 - averageV / 255) * 100
     normalizedV.toInt
   }
 
-  def readPhotoPaths(input: String): ParArray[String] = {
-    new File(input).listFiles.par.map(x => x.toString)
-  }
+  def findV_UDF: UserDefinedFunction = udf[Int, Int, Int, Int, Array[Byte]](findV)
 
-  def createNewPaths(sources: ParArray[String], output: String, threshold: Array[String]): ParArray[String] = {
-    sources.map(
-      x => {
-        val value = findV(x)
-        val label = if (value >= threshold(0).toInt) "dark" else "bright"
-        val extension = x.split("\\.")
-        val filename = extension(0).split("/")
-        val outFolder = if (!output.endsWith("/")) output + "/" else output
-        outFolder + filename.last + "_" + label + "_" + value + "." + extension(1)
-      })
-  }
+  def fixPath: String => String = (x: String) => x.substring(dropLeft, x.length)
+
+  def fixPath_UDF: UserDefinedFunction = udf[String, String](fixPath)
 
   def main(args: Array[String]): Unit = {
-    val config = ConfigFactory.load()
 
-    val input = config.getString("conf.in")
-    val output = config.getString("conf.out")
+    def classifier(path: String, value: Int): String = {
+      val label = if (value >= args(0).toInt) "dark" else "bright"
+      val extension = path.split("\\.")
+      val name = extension(0).split("/").last
+      output + "/" + name + "-" + value + "-" + label + "." + extension(1)
+    }
 
-    val sources = readPhotoPaths(input)
-    val taggedSources = createNewPaths(sources, output, args)
+    def classifier_UDF: UserDefinedFunction = udf[String, String, Int](classifier)
 
-    //Copies images from the 'in' to the 'out' folder (specified in the reference.conf)
-    sources.zip(taggedSources).map({
+    val spark = SparkSession.builder()
+      .master("local")
+      .appName("OpenCV")
+      .getOrCreate()
+
+    val photos = spark.read.format("image")
+      .option("dropInvalid", value = true)
+      .load(input).select("image.*")
+
+    val V = photos
+      .withColumn("V", findV_UDF(photos("height"), photos("width"), photos("nChannels"), photos("data")))
+      .withColumn("fixedPath", fixPath_UDF(photos("origin")))
+
+    val classified = V
+      .withColumn("pathClassified", classifier_UDF(V("fixedPath"), V("V")))
+
+    val copyFrom = classified.select("fixedPath").collect.map(_.getString(0)).toList
+    val copyTo = classified.select("pathClassified").collect.map(_.getString(0)).toList
+
+    copyFrom.zip(copyTo).map({
       case (x, y) =>
         Files.copy(Paths.get(x), Paths.get(y), StandardCopyOption.REPLACE_EXISTING)
     })
+
+    spark.stop()
   }
 }
